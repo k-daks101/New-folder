6d8c55df1437f570962e7a6036c87537998e6c56
Kwame Dako <147068894+k-daks101@users.noreply.github.com>
2024-09-17 21:32:00 +0000

    display new meal and workout

--- JS/js1.js
+++ JS/js1.js
@@ -25,6 +25,7 @@ class CalorieTracker
   {
     this._meals.push(meal);
     this._totalCalories += meal.calories;
+    this._displayNewMeal(meal);
     this._render()
   }
 
@@ -32,6 +33,7 @@ class CalorieTracker
   {
     this._workouts.push(workout);
     this._totalCalories -= workout.calories;
+    this._displayNewWorkout(workout);
     this._render();
   }
 
@@ -110,6 +112,54 @@ class CalorieTracker
     const width = Math.min(percentage, 100);
     progressEl.style.width = `${width}%`
   }
+
+  _displayNewMeal(meal)
+  {
+    const mealsEl = document.getElementById('meal-items');
+    const mealEl = document.createElement('div');
+    mealEl.classList.add('card', 'my-2' );
+    mealEl.setAttribute('data-id', meal.id);
+    mealEl.innerHTML = `
+    <div class="card-body">
+               <div class="d-flex align-items-center justify-content-between">
+                 <h4 class="mx-1">${meal.name}</h4>
+                 <div
+                   class="fs-1 bg-primary text-white text-center rounded-2 px-2 px-sm-5"
+                 >
+                  ${meal.calories}
+                 </div>
+                 <button class="delete btn btn-danger btn-sm mx-2">
+                   <i class="fa-solid fa-xmark"></i>
+                 </button>
+               </div>
+             </div>
+   `;
+    mealsEl.appendChild(mealEl);
+  }
+
+  _displayNewWorkout(Workout)
+  {
+    const workoutsEl = document.getElementById('workout-items');
+    const workoutEl = document.createElement('div');
+    workoutEl.classList.add('card', 'my-2' );
+    workoutEl.setAttribute('data-id', Workout.id);
+    workoutEl.innerHTML = `
+    <div class="card-body">
+               <div class="d-flex align-items-center justify-content-between">
+                 <h4 class="mx-1">${Workout.name}</h4>
+                 <div
+                   class="fs-1 bg-secondary text-white text-center rounded-2 px-2 px-sm-5"
+                 >
+                  ${Workout.calories}
+                 </div>
+                 <button class="delete btn btn-danger btn-sm mx-2">
+                   <i class="fa-solid fa-xmark"></i>
+                 </button>
+               </div>
+             </div>
+   `;
+    workoutsEl.appendChild(workoutEl);
+  }
   _render() 
   {
     this._displayCaloriesTotal();
@@ -153,10 +203,10 @@ class App
     this._tracker  = new CalorieTracker();
 
     document.getElementById('meal-form')
-    .addEventListener('submit', this._newMeal.bind(this));
+    .addEventListener('submit', this._newMeal.bind(this)); //with bind, we can create parameters and pass in arguments
 
     document.getElementById('workout-form')
-    .addEventListener('submit', this._newWorkout.bind(this));
+    .addEventListener('submit', this._newWorkout.bind(this, ));
 
   }
 
